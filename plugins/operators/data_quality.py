37df3ae6b54bfe47f21cc55ed3c660c3883fa788
Ben <mrbena77@gmail.com>
2021-05-05 23:07:12 +0100

    - overhaul of operator to utilise DAG task parameter of dictionary values for dynamic SQL table counts

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -3,33 +3,43 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 import logging
 
-class DataQualityOperator(BaseOperator):
 
+class DataQualityOperator(BaseOperator):
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
                  # operator params (with defaults)
                  redshift_conn_id="",
-                 tables=[],
+                 table_names_expected_records=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Mapped operator params
         self.redshift_conn_id = redshift_conn_id
-        self.tables = tables
+        self.table_names_expected_records = table_names_expected_records
 
     def execute(self, context):
-        #self.log.info('DataQualityOperator not implemented yet')
-        
+        # self.log.info('DataQualityOperator not implemented yet')
+
         self.log.info("Connecting to Redshift database")
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        
-        for table in self.tables:
-            table_records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
-            if len(table_records) < 1 or len(table_records[0]) < 1:
-                raise ValueError(f"Data quality check failed. {table} returned no results")
-            num_records = table_records[0][0]
-            if num_records < 1:
-                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
-            logging.info(f"Data quality on table {table} check passed with {table_records[0][0]} records")
+
+        # for dic in self.table_names_expected_records:
+        #     for key in dic.values():
+        #         record_count = redshift_hook.get_records(f"SELECT COUNT(*) FROM {key}")
+        #         expected_count = 0
+        #         if len(record_count[0]) == expected_count:
+        #             raise ValueError(f"Record count advisory. {key} does not match expected results")
+        #         logging.info(f"Record count advisory. {key} does not match expected results")
+
+        for dic in self.table_names_expected_records:
+            table_name = dic['table']
+            expected_count = dic['expected']
+            record_count = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table_name}")
+            if record_count[0][0] != expected_count:
+                raise ValueError(f"WARNING, {table_name} record count advisory."
+                             f"{record_count[0][0]} does not match expected result {expected_count}!")
+            else:
+                logging.info(f"SUCCESS, {table_name} record count."
+                             f"{record_count[0][0]} matches expected result {expected_count}!")
